80100a5d782921a0d4e4db574cb2f04f04a00aea
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2018-12-26 13:14:14 +0700

    upgrade to angular version 7

---
+++ src/app/Todolist/Todolist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Todolist',
+  templateUrl: './Todolist.component.html',
+  styleUrls: ['./Todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TodolistComponent } from './Todolist/Todolist.component';
+
+import { Routes,RouterModule} from '@angular/router';
+
+const appRoute:Routes=[
+    {path:'',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'Todolist',component:TodolistComponent},
+]
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      LoginComponent,
+      RegisterComponent,
+      TodolistComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot(appRoute),
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
